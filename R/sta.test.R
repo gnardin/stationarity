45cfd08673044fb40a6b296ff30c29f7f2019805
Luis Gustavo Nardin <gnardin@gmail.com>
2018-09-26 11:11:31 +0200

    Change name nosia.test to sta.test

--- R/sta.test.R
+++ R/sta.test.R
@@ -1,51 +1,51 @@
-#' @title Sequential Test Procedure Algorithm Stationarity Test
+#' @title Sequential Testing Algorithm (STA)
 #'
 #' @description
-#' Sequential test procedure algorithm stationarity test.
-#' 
+#' Sequential testing algorithm for stationarity testing.
+#'
 #' @param data Time series data
 #' @param alpha Value of alpha for the statistics test
 #' @param window Structural change window size proportion (0 - 1)
-#' 
+#'
 #' @return ERS, BG, AT, SC, Total results (0: Non-Stationary, 1: Stationary,
 #' NA: Not used test)
-#' 
-#' @export "nosia.test"
-#' 
-nosia.test <- function(data, alpha, window){
-  
+#'
+#' @export "sta.test"
+#'
+sta.test <- function(data, alpha, window){
+
   result <- rep(NA, 5)
-  
+
   test <- elliot.rothenberg.stock.test(data, alpha)
-  
+
   if(!is.na(test)){
     if((!is.na(test)) & (test == NONSTATIONARY)){
       result[1] <- NONSTATIONARY
       result[5] <- NONSTATIONARY
     } else {
       result[1] <- STATIONARY
-      
+
       # BG tests Trend/Break AutoCorrelation
       #          Misclassified Break Mean and Trend Variance
       bg <- breusch.godfrey.test(data, alpha)
-      
+
       if(!is.na(bg)){
         if(bg == NONSTATIONARY){
           result[2] <- NONSTATIONARY
           result[5] <- NONSTATIONARY
         } else {
           result[2] <- STATIONARY
-          
+
           # AT tests Trend/Break Variance
           at <- arch.test(data, alpha)
-          
+
           if(!is.na(at)){
             if(at == NONSTATIONARY){
               result[3] <- NONSTATIONARY
               result[5] <- NONSTATIONARY
             } else {
               result[3] <- STATIONARY
-              
+
               ## Adjust Structure Change test percentage size
               # SC test Break Mean
               if(window > 1){
@@ -53,9 +53,9 @@ nosia.test <- function(data, alpha, window){
               } else if(window < 0){
                 window <- 0
               }
-              
+
               sc <- structure.change.test(data, alpha, "ME", window)
-              
+
               if(!is.na(sc)){
                 if(sc == NONSTATIONARY){
                   result[4] <- NONSTATIONARY
@@ -71,6 +71,6 @@ nosia.test <- function(data, alpha, window){
       }
     }
   }
-  
+
   return(result)
 }
